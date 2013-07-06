1478844e4b3f9a3e32285992ba36feb996947b9b
Drazen Penic <Drazen.Penic@gmail.com>
2013-07-06 22:40:27 +0000

    Added reading from index.html

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var out = fs.readFileSync("index.html");
+  var buffer = new Buffer(out, "utf-8");
+  response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
